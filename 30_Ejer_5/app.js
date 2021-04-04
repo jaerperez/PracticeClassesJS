f749f4d7722a7e2b9e31821c41fbae47b0c6faba
Javier Pérez <jaerperez@gmail.com>
2021-04-04 16:21:42 -0500

    Practice Search2

---
+++ 30_Ejer_5/app.js
@@ -0,0 +1,58 @@
+
+const apiKey = 'EapKeNprg8BFJKA8UTk19tF73ar1hmk6';
+
+
+
+function findMatches(wordToMatch, gifs) {
+    return gifs[0].filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.title.match(regex)
+    });
+}
+
+/*function displayMatches(e) {
+    if (e.target.value == "") {
+        const clear = "";
+        suggestions.innerHTML = clear;
+    } else {
+        const matchedArray = findMatches(e.target.value, gifhys);
+        const html = matchedArray.map(place => {
+            const regex = new RegExp(e.target.value, 'gi');
+            const gifyNames = place.title.replace(regex, `<span class="letters">${e.target.value}</span>`);
+            return `
+        <li>
+            <span class="gifs">${gifyNames}</span>
+        </li>
+    `;
+        }).join('');
+        console.log(html);
+        suggestions.innerHTML = html;
+    }
+}*/
+
+const search = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+//search.addEventListener('change', displayMatches);
+search.addEventListener('keyup', displayMatches);
+
+
+function displayMatches(e) {
+    let valor = 'cake'
+    const endpoint = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + valor + 'j&limit=25&offset=0&rating=g&lang=en';
+    const gifhys = [];
+    console.log(endpoint)
+    fetch(endpoint)
+        .then(blob => blob.json())
+        .then(newdata => gifhys.push(newdata.data));
+    console.log(gifhys)
+
+    if (e.target.value == "") {
+        const clear = "";
+        suggestions.innerHTML = clear;
+    } else {
+        
+
+
+    }
+}
